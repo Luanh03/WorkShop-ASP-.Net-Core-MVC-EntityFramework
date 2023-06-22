c1f14729d28ea5c375ac0aee0b30019374974b9a
Luan Henrique <luanhcosta03@gmail.com>
2023-06-22 12:13:09 -0300

    CRUD Scaffolding

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
