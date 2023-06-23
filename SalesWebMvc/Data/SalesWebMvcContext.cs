098b9a29a505238e51ffcc3f900f7a16c80fa25e
Luan Henrique <luanhcosta03@gmail.com>
2023-06-23 11:41:41 -0300

    Changing theme e Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -15,6 +15,7 @@ namespace SalesWebMvc.Data
         }
 
         public DbSet<Department> Department { get; set; } = default!;
-
+        public DbSet<Seller> Seller { get; set; } = default!;
+        public DbSet<SalesRecord> SalesRecord { get; set; } = default!;
     }
 }
